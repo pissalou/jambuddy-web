db602ef8fa9b70b6e0ab024fc38dace105de3d8e
Pascal Mazars <pascal.mazars@indra.no>
2026-06-07 12:36:06 +0200

    add lookAhead clock syncing audio and MIDI

---
+++ lookahead-worker.js
@@ -0,0 +1,12 @@
+let timerId = null;
+
+self.onmessage = function(e) {
+  if (e.data?.cmd === 'start') {
+    let checkInterval = e.data.interval;
+    console.log(`Lookahead worker started with interval ${checkInterval.toFixed(3)}ms`);
+    timerId = setInterval(() => self.postMessage('tick'), checkInterval);
+  } else if (e.data === 'stop') {
+    clearInterval(timerId);
+    timerId = null;
+  }
+};
